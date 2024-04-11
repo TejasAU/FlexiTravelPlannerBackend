3b5c4d34034a9c382b4e982d5601c3da3e1cfc3b
yashm22 <yashmangle@gmail.com>
2024-04-11 19:26:57 +0530

    Schemas Added

--- DataSchema/userSchema.js
+++ DataSchema/userSchema.js
@@ -1,16 +1,34 @@
 const mongoose = require('mongoose');
 
 // Define the user schema
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+const userSchema = new mongoose.Schema(
+    {
+      username: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+  
+      photo: {
+        type: String,
+      },
+  
+      role: {
+        type: String,
+        default: "user",
+      },
+    },
+    { timestamps: true }
+  );
 
 // Create and export the User model
 module.exports = mongoose.model('User', userSchema);
