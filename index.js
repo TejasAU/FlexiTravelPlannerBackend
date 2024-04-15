e74f220e253982281bfd1a9ad6421dd01267871c
VK <vishanto95@gmail.com>
2024-04-15 16:06:21 +0530

    Logger and mongodb connection added

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
-const mongoose = require('mongoose');
+const app = require('./app') // The Express app
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-// MongoDB connection URI
-const uri = 'mongodb+srv://yash:sem6flexi@cluster0.kdcqihi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-
-// Connect to MongoDB
-mongoose.connect(uri);
-
-// Get the default connection
-const db = mongoose.connection;
-
-// Event listeners for successful connection and error
-db.on('connected', () => {
-  console.log('Connected to MongoDB');
-});
-
-db.on('error', (error) => {
-  console.error('Error connecting to MongoDB:', error);
-});
-
-module.exports = db;
-
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
